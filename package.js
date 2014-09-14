28e47c586f9c5c20bb84c9d471133f839edf711a
Jimmie Butler <jimmiebtlr@gmail.com>
2014-09-14 18:47:20 -0500

    Remove extra folders.

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
   summary: "Vertical Calendar plugin for meteor ",
-  version: "0.0.1-rc.7",
+  version: "0.0.1-rc.8",
   git: "https://github.com/jimmiebtlr/vertiCal.git"
 });
 
 Package.onUse(function(api) {
-  api.use(['templating'],'client');
+  api.use(['templating','mrt:moment@2.6.0'],'client');
   api.versionsFrom('METEOR@0.9.1.1');
   api.addFiles(['vertiCal.html','vertiCal.js','vertiCal.css'],'client');
 });
